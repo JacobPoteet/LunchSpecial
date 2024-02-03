1741cb5367077a89cde7f04a9cdd74b34cff54d5
Jacob Poteet <64765148+JacobPoteet@users.noreply.github.com>
2024-02-03 17:19:22 -0500

    Refactor Guess.svelte for improved user experience
    - Added handleKeyDown function to submit guess on Enter key press
    - Reorganized code for better readability and maintainability

--- lunchspecial/src/routes/Guess.svelte
+++ lunchspecial/src/routes/Guess.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
-    import { createEventDispatcher } from 'svelte';
-    const dispatch = createEventDispatcher();
-  
-    let guess = '';
-  
-    function submitGuess() {
-      dispatch('submitGuess', guess);
-      guess = '';
+  import { createEventDispatcher } from 'svelte';
+  const dispatch = createEventDispatcher();
+
+  let guess = '';
+
+  function submitGuess() {
+    dispatch('submitGuess', guess);
+    guess = '';
+  }
+
+  function handleKeyDown(event : any) {
+    if (event.key === 'Enter') {
+      submitGuess();
     }
-  </script>
-  
-<input bind:value={guess} on:keydown|preventDefault={submitGuess}>
-  <button on:click={submitGuess}>Guess</button>
+  }
+
+</script>
+
+<input bind:value={guess} on:keydown={handleKeyDown} />
+<button on:click={submitGuess}>Guess</button>
